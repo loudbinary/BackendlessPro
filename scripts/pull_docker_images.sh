5939b32d586e89cb2db79365629dbf28720ddf3d
Loudbinary <loudbinary@gmail.com>
2019-01-12 04:48:47 -0500

    Update pull_docker_images.sh

--- scripts/pull_docker_images.sh
+++ scripts/pull_docker_images.sh
@@ -3,6 +3,7 @@
 EMPTY_MOUNTS_CHECKSUM="56a5457830766120cc4a508b526c0356"
 # Checks state of current BackendlessPro/scripts/mounts directory, and generates hash
 # If hash matches above, directory is empty, and we need to run backendless_install.sh script
+sudo apt-get install md5deep -y
 md5deep -r BackendlessPro/scripts/mounts > dirlist_checksum.md5
 cat dirlist_checksum.md5  | tr -d '\n' > oneline.txt
 md5sum -b oneline.txt | cut -d" " -f1 > checksum.md5
