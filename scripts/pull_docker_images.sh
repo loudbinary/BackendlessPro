9a4abead0180960119b0a01af7631bd33ec0dfa8
Loudbinary <loudbinary@gmail.com>
2019-01-12 04:36:08 -0500

    Create pull_docker_images.sh

---
+++ scripts/pull_docker_images.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+EMPTY_MOUNTS_CHECKSUM="56a5457830766120cc4a508b526c0356"
+# Checks state of current BackendlessPro/scripts/mounts directory, and generates hash
+# If hash matches above, directory is empty, and we need to run backendless_install.sh script
+md5deep -r BackendlessPro/scripts/mounts > dirlist_checksum.md5
+cat dirlist_checksum.md5  | tr -d '\n' > oneline.txt
+md5sum -b oneline.txt | cut -d" " -f1 > checksum.md5
+rm dirlist_checksum.md5 && rm oneline.txt
+CURRENT_CHECKSUM=$(cat checksum.md5)
+
+
+if [ "$EMPTY_MOUNTS_CHECKSUM" = "$CURRENT_CHECKSUM" ]; then
+        echo "Bootstraping fresh swarm"
+        chmod +x BackendlessPro/scripts/*.sh
+        cd BackendlessPro/scripts
+        ./backendless_install.sh
+
+else
+        echo "Changes in mounts directory"
+fi
