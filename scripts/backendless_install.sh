5bcc7d599c82e9fbfebf78c3136a34572bd21c23
Loudbinary <loudbinary@gmail.com>
2019-01-12 03:56:08 -0500

    Update backendless_install.sh

--- scripts/backendless_install.sh
+++ scripts/backendless_install.sh
@@ -17,4 +17,12 @@ if [[ "$registry" == "backendless"  ]]; then
   ./pull.sh ${version} backendless
 fi
 
+cat >> ports.env <<EOL
+BL_MYSQL_PORT=3306
+BL_MONGODB_PORT=27017
+BL_PROPERTY_config_redis_bl_debug_port=6380
+BL_PROPERTY_config_server_publicPort=9000
+BL_PROPERTY_config_console_port=80
+BL_PROPERTY_config_rt_server_socketServer_connection_port=5000
+EOL
 #./check_ports.sh
